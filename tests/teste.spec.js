366700beca52c3d0f2b15cc15f658cf3da065452
Adriel Pereira Siqueira <adrielsiqueira@plusoft.com>
2026-03-25 18:53:29 -0300

    inicio teste 2

--- tests/teste.spec.js
+++ tests/teste.spec.js
@@ -13,7 +13,30 @@ test.describe('Validação do site', () => {
        await expect(page.getByText('Hulk', {exact : true})).toBeVisible();  
     });
 
-    test('busca da primeira linha')
+  
 
-});
+    test('verify your account', async ({page}) => {
+        await page.goto('https://qaplayground.dev/apps/verify-account/');
+        // await page.goto('https://qaplayground.dev/');
+        // await page.getByRole('link', { name: 'Mini Web Apps' }).click();
+        // await page.getByRole('link', { name: 'Verify Your Account Enter' }).click();
+        const infoElement = page.locator('small.info');
+        const rawText = await infoElement.textContent();
+        console.log(rawText)
+        const confirmationCode = rawText.match(/\d/g)?.join('') || '';
+        console.log(confirmationCode)
+        console.log(confirmationCode[0])
+        
+        await page.getByPlaceholder('0').first().fill(confirmationCode[0])
+        await page.keyboard.press('Tab');
+        
+        for(let i = 1;i < 7 ;i++){
+            await page.keyboard.type(confirmationCode[i]);
+            await page.waitForTimeout(1000);
+            console.log(i)
+        }
+    
+        await expect(page.getByText('Success')).toBeVisible();
+    });     
 
+})
